dec26eb29df7c2d9fb79391b1786b340c1966152
ticoerrecart <ticoerrecart@5a6bf194-4179-11de-9805-9778bc541ee5>
2009-09-23 21:32:53 +0000

    Archivos comprimidos

--- src/rytsa/documentador/Documentador.java
+++ src/rytsa/documentador/Documentador.java
@@ -3,9 +3,11 @@ package rytsa.documentador;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.tools.JavaCompiler;
@@ -19,7 +21,7 @@ public class Documentador {
 	public String proyecto;
 	public String separador;
 	private Analizador analizador;// = new Analizador();
-	
+
 	public String getProyecto() {
 		return proyecto;
 	}
@@ -43,30 +45,63 @@ public class Documentador {
 	public void setFileOrDirectory(File fileOrDirectory) {
 		this.fileOrDirectory = fileOrDirectory;
 	}
-	
-	
-	private void recuperarFiles(File file, List<File> filesSalida){
-			if (file.isDirectory()){
-				File allFiles[] = file.listFiles();
-				for (File file2 : allFiles) {
-					recuperarFiles(file2, filesSalida);
-				}
-			} else {
-				if (file.getName().endsWith(".java")){
+
+	private void descomprimirZip(File file, List<File> filesSalida)
+			throws IOException {
+		// FileUtil.getInstance().decompressZipFile(file.getAbsolutePath(),
+		// "c:\\documentador\\");
+		Enumeration entries;
+		ZipFile zipFile = null;
+		File rootDir = null;
+		zipFile = new ZipFile(file);
+		// processing zip file entries
+		entries = zipFile.entries();
+		while (entries.hasMoreElements()) {
+			ZipEntry entry = (ZipEntry) entries.nextElement();
+
+			if (!entry.isDirectory()) {
+				if (seDebeAgregarFile(file)) {
 					filesSalida.add(file);
 				}
 			}
-		
+		}
 	}
-	
-	public void documentar(){
-		//recorro todo los archivos dado un zip un file o un directorio/ por ahora solo .java
-		List<File> files = new ArrayList<File>(); 
-		recuperarFiles(fileOrDirectory,files);
 
-		
-//		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-		
+	private boolean seDebeAgregarFile(File file) {
+		return file.getName().endsWith(".java");
+	}
+
+	private void recuperarFiles(File file, List<File> filesSalida)
+			throws IOException {
+		if (file.isDirectory()) {
+			File allFiles[] = file.listFiles();
+			for (File file2 : allFiles) {
+				recuperarFiles(file2, filesSalida);
+			}
+		} else {
+			if (seDebeAgregarFile(file)) {
+				filesSalida.add(file);
+			} else {
+				if (file.getName().endsWith(".zip")
+						|| file.getName().endsWith(".rar")
+						|| file.getName().endsWith(".jar")
+						|| file.getName().endsWith(".ear")
+						|| file.getName().endsWith(".war")) {
+					descomprimirZip(file, filesSalida);
+				}
+			}
+		}
+
+	}
+
+	public void documentar() throws IOException {
+		// recorro todo los archivos dado un zip un file o un directorio/ por
+		// ahora solo .java
+		List<File> files = new ArrayList<File>();
+		recuperarFiles(fileOrDirectory, files);
+
+		// JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
 		// Try finding a compiler.
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 		if (compiler == null) {
@@ -74,7 +109,8 @@ public class Documentador {
 			final ClassLoader loader = Documentador.class.getClassLoader();
 			Class<JavaCompiler> cls;
 			try {
-				cls = (Class<JavaCompiler>) loader.loadClass("com.sun.tools.javac.api.JavacTool");
+				cls = (Class<JavaCompiler>) loader
+						.loadClass("com.sun.tools.javac.api.JavacTool");
 				compiler = cls.asSubclass(JavaCompiler.class).newInstance();
 			} catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
@@ -86,28 +122,30 @@ public class Documentador {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
 
-		
-		StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
-		File filesArray[] = new File[]{}; 
+		StandardJavaFileManager fileManager = compiler.getStandardFileManager(
+				null, null, null);
+		File filesArray[] = new File[] {};
 		filesArray = files.toArray(filesArray);
-		Iterable<? extends JavaFileObject> compilationUnits1 = fileManager.getJavaFileObjects(filesArray); 
+		Iterable<? extends JavaFileObject> compilationUnits1 = fileManager
+				.getJavaFileObjects(filesArray);
 
-		CompilationTask task = compiler.getTask(null, fileManager, null, null, null, compilationUnits1);
+		CompilationTask task = compiler.getTask(null, fileManager, null, null,
+				null, compilationUnits1);
 		// Get the list of annotation processors
 		LinkedList<AbstractProcessor> processors = new LinkedList<AbstractProcessor>();
 		analizador = new Analizador();
 		processors.add(analizador);
 		task.setProcessors(processors);
-		// 	Perform the compilation task.
-		
+		// Perform the compilation task.
+
 		task.call();
-		
-		
+
 		try {
-			Exportador.exportar(this.getSeparador(),this.getProyecto(),this.fileOrDirectory.getName(),this.analizador.beans);
+			Exportador.exportar(this.getSeparador(), this.getProyecto(),
+					this.fileOrDirectory.getName(), this.analizador.beans);
 			fileManager.close();
 		} catch (IOException e) {
 			System.out.println(e.getLocalizedMessage());
