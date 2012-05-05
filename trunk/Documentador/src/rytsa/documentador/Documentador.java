e83d75f212496cd36ba86fce5de1e81f3c25c0d2
fabiana.etchepare@gmail.com <fabiana.etchepare@gmail.com@5a6bf194-4179-11de-9805-9778bc541ee5>
2012-05-05 22:19:41 +0000

    Entrando en detalles mayo/2012

--- trunk/Documentador/src/rytsa/documentador/Documentador.java
+++ trunk/Documentador/src/rytsa/documentador/Documentador.java
@@ -30,17 +30,18 @@ public class Documentador {
 	private String tabla5;
 	protected static String extensionesAScanear;
 	protected static String extensionesAInventariar;
-	
+
 	protected static ResourceBundle resourceBundle;
-	
+
 	List<File> files = new ArrayList<File>();
 
 	static {
 		resourceBundle = ResourceBundle.getBundle("config");
-		extensionesAInventariar = resourceBundle.getString("extensionesAInventariar");
+		extensionesAInventariar = resourceBundle
+				.getString("extensionesAInventariar");
 		extensionesAScanear = resourceBundle.getString("extensionesAScanear");
 	}
-	
+
 	public String getTabla1() {
 		return tabla1;
 	}
@@ -51,10 +52,14 @@ public class Documentador {
 
 	public Documentador() {
 		Calendar cal = Calendar.getInstance();
-		
-		tabla1 =  "OJ-" + Integer.toString(cal.get(Calendar.DATE)) + "-" + Integer.toString(cal.get(Calendar.MONTH)+1) + "-" + Integer.toString(cal.get(Calendar.YEAR)) + ".txt";
-		tabla5 =  "CR-" + Integer.toString(cal.get(Calendar.DATE)) + "-" + Integer.toString(cal.get(Calendar.MONTH)+1) + "-" + Integer.toString(cal.get(Calendar.YEAR)) + ".txt";
-	
+
+		tabla1 = "OJ-" + Integer.toString(cal.get(Calendar.DATE)) + "-"
+				+ Integer.toString(cal.get(Calendar.MONTH) + 1) + "-"
+				+ Integer.toString(cal.get(Calendar.YEAR)) + ".txt";
+		tabla5 = "CR-" + Integer.toString(cal.get(Calendar.DATE)) + "-"
+				+ Integer.toString(cal.get(Calendar.MONTH) + 1) + "-"
+				+ Integer.toString(cal.get(Calendar.YEAR)) + ".txt";
+
 	}
 
 	public String getProyecto() {
@@ -81,8 +86,8 @@ public class Documentador {
 		this.fileOrDirectory = fileOrDirectory;
 	}
 
-	private void descomprimirZip(File file, List<File> filesSalida, String extensionDeArchivo)
-			throws IOException {
+	private void descomprimirZip(File file, List<File> filesSalida,
+			String extensionDeArchivo) throws IOException {
 		// FileUtil.getInstance().decompressZipFile(file.getAbsolutePath(),
 		// "c:\\documentador\\");
 		Enumeration entries;
@@ -105,19 +110,20 @@ public class Documentador {
 	private boolean seDebeAgregarFile(File file, String extensionDeArchivo) {
 		boolean agregar = false;
 		for (String ext : extensionDeArchivo.split(",")) {
-			if ((file.getName().toLowerCase()).endsWith((ext.trim()).toLowerCase())){
+			if ((file.getName().toLowerCase()).endsWith((ext.trim())
+					.toLowerCase())) {
 				agregar = true;
 			}
 		}
-		return agregar; 
+		return agregar;
 	}
 
-	private void recuperarFiles(File file, List<File> filesSalida, String extensionDeArchivo)
-			throws IOException {
+	private void recuperarFiles(File file, List<File> filesSalida,
+			String extensionDeArchivo) throws IOException {
 		if (file.isDirectory()) {
 			File allFiles[] = file.listFiles();
 			for (File file2 : allFiles) {
-				recuperarFiles(file2, filesSalida, extensionDeArchivo );
+				recuperarFiles(file2, filesSalida, extensionDeArchivo);
 			}
 		} else {
 			if (seDebeAgregarFile(file, extensionDeArchivo)) {
@@ -138,7 +144,7 @@ public class Documentador {
 	public void documentar() throws IOException {
 		// recorro todo los archivos dado un zip un file o un directorio/ por
 		// ahora solo .java
-		
+
 		recuperarFiles(fileOrDirectory, files, ".java");
 
 		// JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
@@ -173,10 +179,12 @@ public class Documentador {
 		Iterable<? extends JavaFileObject> compilationUnits1 = fileManager
 				.getJavaFileObjects(filesArray);
 
-		//CompilationTask task = compiler.getTask(null, fileManager, null, null,null, compilationUnits1);
-		String[] options =  {"-Xmaxerrs","500"};
-		CompilationTask task = compiler.getTask(null, fileManager, null , Arrays.asList(options) , null, compilationUnits1);
-	
+		// CompilationTask task = compiler.getTask(null, fileManager, null,
+		// null,null, compilationUnits1);
+		String[] options = { "-Xmaxerrs", "500" };
+		CompilationTask task = compiler.getTask(null, fileManager, null,
+				Arrays.asList(options), null, compilationUnits1);
+
 		// Get the list of annotation processors
 		LinkedList<AbstractProcessor> processors = new LinkedList<AbstractProcessor>();
 		analizador = new Analizador();
@@ -186,96 +194,110 @@ public class Documentador {
 
 		task.call();
 		fileManager.close();
-		
+
 		try {
-			//Voy a ampliar la info de los beans, agregando los select, insert, delete como referencias y las tablas como objetos en el inventario
+			// Voy a ampliar la info de los beans, agregando los select, insert,
+			// delete como referencias y las tablas como objetos en el
+			// inventario
 			buscoReferenciasSQL();
-			
+
+			// Voy a ampliar la info de los beans, agregando los imports;
+			buscoReferenciasImport();
+
 			ampliarDocumentacion(fileOrDirectory, files);
-			
+
 			Exportador.exportar(this.getSeparador(), this.getProyecto(),
-					this.fileOrDirectory.getName(), this.analizador.beans, this.tabla1, this.tabla5);
-			
+					this.fileOrDirectory.getName(), this.analizador.beans,
+					this.tabla1, this.tabla5);
+
 		} catch (IOException e) {
 			System.out.println(e.getLocalizedMessage());
 		}
-		
+
 		files.clear();
 	}
-	
+
 	// Creo nuevos beans con archivos que no son ".java"
-	private void ampliarDocumentacion(File file, List<File> filesSalida){
+	private void ampliarDocumentacion(File file, List<File> filesSalida) {
 		try {
 			int cantFilesAnt = files.size();
 			recuperarFiles(fileOrDirectory, files, this.extensionesAInventariar);
 			int cantFilesAct = files.size();
 			String codigoFuente = "";
 			String codigoFuenteAux;
-			
-			//Por cada archivo (los no java) que se haya agregado a la documentación, tengo que obtener info
-			List<File> salidaAmpliada = filesSalida.subList(cantFilesAnt, cantFilesAct);
-			
+
+			// Por cada archivo (los no java) que se haya agregado a la
+			// documentación, tengo que obtener info
+			List<File> salidaAmpliada = filesSalida.subList(cantFilesAnt,
+					cantFilesAct);
+
 			for (File file2 : salidaAmpliada) {
 				ClaseBean beanNoJava = new ClaseBean();
 				int ubicacionPunto = file2.getName().lastIndexOf(".");
-								
-				beanNoJava.setPaquete(file2.getParent());				
-				beanNoJava.setNombre(file2.getName().substring(0, ubicacionPunto));
-				beanNoJava.setTipo(file2.getName().substring(ubicacionPunto+1));
-				beanNoJava.setSubtipo(file2.getName().substring(ubicacionPunto+1));
-				beanNoJava.setDescripcion("");			
-				
-				// Si el archivo es de uno de los tipos a scanear, lee y guarda el código en el bean
-				for (String ext : this.extensionesAScanear.split(",")) {
-					if ((file2.getName().toLowerCase()).endsWith((ext.trim()).toLowerCase())){
-					
-						BufferedReader bf = new BufferedReader(new FileReader(file2));
 
-						while ((codigoFuenteAux = bf.readLine())!=null) {
-						   codigoFuente = codigoFuente + codigoFuenteAux;
-						   // System.out.println(codigoFuenteAux);
+				beanNoJava.setPaquete(file2.getParent());
+				beanNoJava.setNombre(file2.getName().substring(0,
+						ubicacionPunto));
+				beanNoJava.setTipo(file2.getName()
+						.substring(ubicacionPunto + 1));
+				beanNoJava.setSubtipo(file2.getName().substring(
+						ubicacionPunto + 1));
+				beanNoJava.setDescripcion("");
+
+				// Si el archivo es de uno de los tipos a scanear, lee y guarda
+				// el código en el bean
+				for (String ext : this.extensionesAScanear.split(",")) {
+					if ((file2.getName().toLowerCase()).endsWith((ext.trim())
+							.toLowerCase())) {
+
+						BufferedReader bf = new BufferedReader(new FileReader(
+								file2));
+
+						while ((codigoFuenteAux = bf.readLine()) != null) {
+							codigoFuente = codigoFuente + codigoFuenteAux;
+							// System.out.println(codigoFuenteAux);
 						}
-						beanNoJava.setCodigoFuente(codigoFuente);				
+						beanNoJava.setCodigoFuente(codigoFuente);
 					}
-				}				
-				this.analizador.beans.add(beanNoJava);			
+				}
+				this.analizador.beans.add(beanNoJava);
 			}
-			
+
 		} catch (IOException e) {
 			System.out.println(e.getLocalizedMessage());
 		}
 	}
-	
-	private void buscoReferenciasSQL(){
-		// 
-		ArrayList<ClaseBean> beansSQL = new ArrayList<ClaseBean>(); //para inventariar las tablas
-		
-		//recorro la colección de beans para examinar su código
+
+	private void buscoReferenciasSQL() {
+		// para inventariar las tablas
+		ArrayList<ClaseBean> beansSQL = new ArrayList<ClaseBean>();
+
+		// recorro la colección de beans para examinar su código
 		for (ClaseBean bean : this.analizador.beans) {
-			
+
 			String codigo = (bean.getCodigoFuente()).toLowerCase();
 			String desconocido = "desconocido";
 			String objeto = "Tabla";
-			
+
 			for (String lineaCodigo : codigo.split(";")) {
 				int ubicFrom = 0;
-				int ubicProximoEspacio=0;
+				int ubicProximoEspacio = 0;
 				String tabla = "";
 				String cardinalidad = "";
-//System.out.println(lineaCodigo);
+
 				if (lineaCodigo.contains("select ")) {
 					ubicFrom = lineaCodigo.indexOf("from ") + 5;
 					ubicProximoEspacio = lineaCodigo.indexOf(" ", ubicFrom);
 					if (ubicProximoEspacio == -1)
-						ubicProximoEspacio = lineaCodigo.length();					
+						ubicProximoEspacio = lineaCodigo.length();
 					tabla = lineaCodigo.substring(ubicFrom, ubicProximoEspacio);
 					cardinalidad = "INP";
-				}				
+				}
 				if (lineaCodigo.contains("update ")) {
-					ubicFrom = lineaCodigo.indexOf("update ")  + 7;
+					ubicFrom = lineaCodigo.indexOf("update ") + 7;
 					ubicProximoEspacio = lineaCodigo.indexOf(" ", ubicFrom);
 					if (ubicProximoEspacio == -1)
-						ubicProximoEspacio = lineaCodigo.length();					
+						ubicProximoEspacio = lineaCodigo.length();
 					tabla = lineaCodigo.substring(ubicFrom, ubicProximoEspacio);
 					cardinalidad = "UPD";
 				}
@@ -283,40 +305,103 @@ public class Documentador {
 					ubicFrom = lineaCodigo.indexOf("from ") + 5;
 					ubicProximoEspacio = lineaCodigo.indexOf(" ", ubicFrom);
 					if (ubicProximoEspacio == -1)
-						ubicProximoEspacio = lineaCodigo.length();					
+						ubicProximoEspacio = lineaCodigo.length();
 					tabla = lineaCodigo.substring(ubicFrom, ubicProximoEspacio);
 					cardinalidad = "UPD";
-					
+
 				}
 				if (lineaCodigo.contains("insert ")) {
 					ubicFrom = lineaCodigo.indexOf("into ") + 5;
 					ubicProximoEspacio = lineaCodigo.indexOf(" ", ubicFrom);
 					if (ubicProximoEspacio == -1)
-						ubicProximoEspacio = lineaCodigo.length();					
+						ubicProximoEspacio = lineaCodigo.length();
 					tabla = lineaCodigo.substring(ubicFrom, ubicProximoEspacio);
 					cardinalidad = "UPD";
 				}
-				if (! "".equals(tabla) && ! "+".equals(tabla) && ! tabla.equals(String.valueOf('"'))) {
+				if (!"".equals(tabla) && !"+".equals(tabla)
+						&& !tabla.equals(String.valueOf('"'))) {
 					ClaseBean cb = new ClaseBean();
 					cb.setNombre(tabla);
 					cb.setCardinalidad(cardinalidad);
-					cb.setPaquete("desconocido");
+					cb.setPaquete(desconocido);
 					cb.setTipo(objeto);
 					cb.setSubtipo(objeto);
 					cb.setDescripcion("");
-					
-					//	no estoy pudiendo detectar cuando el objeto está ya en el list para no insertarlo nuevamente					
-					if (!beansSQL.contains(cb)) {
-						beansSQL.add(cb);						
+
+					if (!existeBean(beansSQL, cb) && !existeBean(this.analizador.beans, cb)) {
+						beansSQL.add(cb);
 					}
-					//	no estoy pudiendo detectar cuando el objeto está ya en el list para no insertarlo nuevamente
-					if (!bean.getReferencias().contains(cb)) { 							
+
+					// Creo que igual se me duplican las referencias aunque use contains pero espero a que Hector pida corrección.	
+					if (!bean.getReferencias().contains(cb)) {
 						bean.getReferencias().add(cb);
-					}	
+					}
 				}
-			}			
+			}
 		}
 		// Agrego los objetos sql a la lista de beans
 		this.analizador.beans.addAll(beansSQL);
 	}
+
+	private void buscoReferenciasImport() {
+		ArrayList<ClaseBean> beansImp = new ArrayList<ClaseBean>();
+		String nombre="";
+
+		// recorro la colección de beans para examinar su código
+		for (ClaseBean bean : this.analizador.beans) {
+
+			if (!bean.getCodigoFuente().equals("")) {
+
+				String codigo = bean.getCodigoFuente();
+
+				for (String lineaCodigo : codigo.split(";")) {
+
+					int ubicImport = 0;
+					int ubicUltimoPunto = 0;
+
+					if (lineaCodigo.contains("import ")) {
+
+						ubicImport = lineaCodigo.indexOf("import ") + 7;
+						ubicUltimoPunto = lineaCodigo.lastIndexOf(".");
+						nombre = (lineaCodigo.substring(
+								ubicUltimoPunto + 1, lineaCodigo.length()))
+								.trim();
+							
+						if (!"*".equals(nombre)) {	
+							ClaseBean cb = new ClaseBean();
+							cb.setNombre(nombre);
+							cb.setCardinalidad("?");
+							cb.setPaquete((lineaCodigo.substring(ubicImport,
+									ubicUltimoPunto)).trim());
+							cb.setDescripcion("");
+	
+							if (!existeBean(beansImp, cb) &&  !existeBean(this.analizador.beans, cb)) {
+								beansImp.add(cb);
+							}
+						
+							bean.getReferencias().add(cb);
+						}
+					}
+				}
+			}
+		}
+		// Agrego los objetos import a la lista de beans
+		this.analizador.beans.addAll(beansImp);
+
+	}
+
+	private boolean existeBean(List<ClaseBean> listaDeBeans , ClaseBean newBean) {
+
+		for (ClaseBean b : listaDeBeans) {
+			if (newBean.getNombre().equalsIgnoreCase(b.getNombre())
+					&& newBean.getPaquete().equalsIgnoreCase(b.getPaquete())
+					&& newBean.getTipo().equalsIgnoreCase(b.getTipo())
+					&& newBean.getSubtipo().equalsIgnoreCase(b.getSubtipo())) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
 }
